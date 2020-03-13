817ace5147d1a9b1d161d391dfde3c9a5149dc27
Adrien BRICCHI <adrien.bricchi@libriciel.coop>
2020-03-13 16:47:40 +0100

    Licence

--- src/test/scala/coop/libriciel/iparapheur/auth/DesksSimulation.scala
+++ src/test/scala/coop/libriciel/iparapheur/auth/DesksSimulation.scala
@@ -1,6 +1,6 @@
 /*
  * i-Parapheur
- * Copyright (C) 2018-2019 Libriciel-SCOP
+ * Copyright (C) 2019-2020 Libriciel-SCOP
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
