133b7f3bce236192aad80805aa264dce09165c27
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2021-04-30 14:12:14 +0200

    WIP: tests d'API, titre et scenario outline.

--- src/test/resources/lib/common.js
+++ src/test/resources/lib/common.js
@@ -1,12 +1,18 @@
 function fn(config) {
 
     config['scenario'] = {'outline': {}};
+    config.scenario.outline['existing'] = function(exists){
+        return exists == true ? 'an existing' : 'a non existing';
+    };
     config.scenario.outline['role'] = function(role){
         return role !== '' && role !== null ? 'a user with the role "' + role + '"' : 'an unauthenticated user';
     };
     config.scenario.outline['status'] = function(status){
         return String(status).match(/^[2-3]/) ? 'can' : 'cannot';
     };
+    // @todo: deprecate
+    config['title'] = config.scenario.outline;
+    config['title']['can'] = config.title.status;
 
     config['pause'] = function(seconds){
         java.lang.Thread.sleep(seconds * 1000);
