05850d590b7768e534cadff23e66876d5e4836b7
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2021-12-15 14:34:27 +0100

    Correction et mise à jour des tests passants sur ip-core.

---
+++ src/test/resources/fixtures/ip-core/v1/admin-workflow-controller/post/data-validation/xxx.js
@@ -0,0 +1,17 @@
+{
+    "steps": [
+    {
+        "validators": [
+            "#(existingDeskId)"
+        ],
+        "validationMode": "SIMPLE",
+        "name": "SEAL",
+        "type": "SEAL",
+        "parallelType": "OR"
+    }
+],
+    "name": "Cachet serveur - #(existingDeskId)",
+    "id": "#(key)",
+    "key": "#(key)",
+    "deploymentId": "#(key)"
+}
