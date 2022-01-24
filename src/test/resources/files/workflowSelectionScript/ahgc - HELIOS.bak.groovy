3fe4fd5e88951e5e6b6b0e82716740fbd73298ca
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2022-01-24 16:42:15 +0100

    Début de l'ajout de dossiers multidoc pour l'entité "Marchés publics"

--- src/test/resources/files/workflowSelectionScript/ahgc - HELIOS.bak.groovy
+++ src/test/resources/files/workflowSelectionScript/ahgc - HELIOS.bak.groovy
@@ -1,4 +1,6 @@
 // org.postgresql.util.PSQLException: ERROR: value too long for type character varying(255)
+// je pourrais juste sélectionner le circuit, s'il n'a pas de bureau variable
+// si je sélectionne le circuit, je lui passe un tableau indexé avec les différents bureaux variables
 selectionCircuit {
     circuit "signature", ["Bleu"];
     //if (GdaBjType % 3 == 0) {
