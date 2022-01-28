87cedfa4085faf9eb1444a25bfecfacd3593ac75
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2022-01-28 09:03:25 +0100

    Correction

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -29,6 +29,7 @@ function fn() {
         //@fixme: APPLICATION_HOST, APPLICATION_PROTOCOL and CHROME_BIN -> null ?
         //baseUrl: java.lang.System.getenv('APPLICATION_PROTOCOL') + '://' + java.lang.System.getenv('APPLICATION_HOST'),
         baseUrl: 'http://iparapheur.dom.local/',
+        // baseUrl: 'https://iparapheur-5-0.dev.libriciel.net/',
         // baseUrl: 'https://iparapheur-5-0.recette.libriciel.net/',
         // CHROME_BIN: java.lang.System.getenv('CHROME_BIN'),
         CHROME_BIN: '/usr/bin/chromium-browser',
