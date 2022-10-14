700d2cec16f4749ac9bf194b212d4994fa026124
tilo <tilo.papin@libriciel.coop>
2022-10-14 14:36:38 +0200

    dump doc + dump script

---
+++ DUMP.md
@@ -0,0 +1,47 @@
+# Database backups
+
+## Docker volumes save
+
+### Dump all data:
+
+```bash
+sudo ./dump.sh <POSTGRES_USER> <POSTGRES_PASSWORD>
+```
+
+### Restore dump :
+
+1. Shutdown all containers.
+   ```bash
+      sudo docker compose down -v
+   ```
+2. Unzip the archived dump.
+   ```bash
+      sudo tar -xf <PATH_TO_DUMP>
+   ```
+
+3. Replace the ./data folder with the data_<date of the dump> folder in the unzipped dump.
+   ```bash
+      sudo rm -r ./data
+      sudo mv -r ./data_<date of the dump>/data_<date of the dump>/ ./data
+   ```
+
+4. Start the Postgres database
+   ```bash
+      sudo docker compose up -d postgres matomo-db
+   ```
+
+5. Load the databses dumps
+   ```bash
+      cat matomo-backup.sql | sudo docker exec -i iparapheur-matomo-db-1 /usr/bin/mysql -u <MATOMO_DB_USER> --password=<MATOMO_DB_PASSWORD> <MATOMO_DB_DATABASE>
+      sudo docker exec iparapheur-postgres-1 /bin/bash -c "PGPASSWORD=<POSTGRES_PASSWORD> psql --username <POSTGRES_USER> alfresco" <"./data_<date of the dump>/postgres-backup-alfresco.sql"
+      sudo docker exec iparapheur-postgres-1 /bin/bash -c "PGPASSWORD=<POSTGRES_PASSWORD> psql --username <POSTGRES_USER> flowable" <"./data_<date of the dump>/postgres-backup-flowable.sql"
+      sudo docker exec iparapheur-postgres-1 /bin/bash -c "PGPASSWORD=<POSTGRES_PASSWORD> psql --username <POSTGRES_USER> ipcore" <"./data_<date of the dump>/postgres-backup-ipcore.sql"
+      sudo docker exec iparapheur-postgres-1 /bin/bash -c "PGPASSWORD=<POSTGRES_PASSWORD> psql --username <POSTGRES_USER> keycloak" <"./data_<date of the dump>/postgres-backup-keycloak.sql"
+      sudo docker exec iparapheur-postgres-1 /bin/bash -c "PGPASSWORD=<POSTGRES_PASSWORD> psql --username <POSTGRES_USER> pastellconector" <"./data_<date of the dump>/postgres-backup-pastellconector.sql"
+      sudo docker exec iparapheur-postgres-1 /bin/bash -c "PGPASSWORD=<POSTGRES_PASSWORD> psql --username <POSTGRES_USER> quartz" <"./data_<date of the dump>/postgres-backup-quartz.sql"
+   ```
+
+6. Restart all conatiners
+   ```bash
+      sudo docker-compose up -d
+   ```
---
+++ dump.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+#
+# iparapheur
+# Copyright (C) 2019-2022 Libriciel SCOP
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+# ----------------------------------------------------------------------------------------------------------------------
+# "Bootstrap"
+# ----------------------------------------------------------------------------------------------------------------------
+
+set -o errexit
+set -o errtrace
+set -o functrace
+set -o nounset
+set -o pipefail
+
+# Use xtrace ?
+if [ "$(getopt --longoptions xtrace -- x "$@" 2>/dev/null | grep --color=none "\(^\|\s\)\(\-x\|\-\-xtrace\)\($\|\s\)")" != "" ]; then
+  set -o xtrace
+fi
+
+POSTGRES_USER=$1
+POSTGRES_PASSWORD=$2
+
+printf "POSTGRES_USER : %s\n" POSTGRES_USER
+printf "POSTGRES_PASSWORD : %s\n" POSTGRES_PASSWORD
+
+CONTAINER_NAME="iparapheur-postgres-1"
+
+DB_NAMES=("alfresco" "flowable" "keycloak" "ipcore" "quartz" "pastellconnector")
+
+# ======================================================================================================================
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Main function
+# ----------------------------------------------------------------------------------------------------------------------
+__main__() {
+  CURRENT_DATE=$(date "+%F_%T")
+  CURRENT_DATE=${CURRENT_DATE//:/-}
+  DUMP_PATH="data_${CURRENT_DATE}"
+
+  mkdir -m 757 "${DUMP_PATH}"
+
+  cp -r data "${DUMP_PATH}"
+  rm -r -f "${DUMP_PATH}"/matomo-db "${DUMP_PATH}"/postgres
+
+  printf "Dumping MatomoDB databases"
+  docker exec iparapheur-matomo-db-1 /usr/bin/mysqldump -u matomo --password=matomo matomo > "${DUMP_PATH}/matomo-backup.sql"
+
+  printf "Dumping PostgreSQL databases"
+
+  for DB_NAME in "${DB_NAMES[@]}"; do
+    printf "Dumping %s...\n" "${DB_NAME}"
+    docker exec ${CONTAINER_NAME} /bin/bash -c "export PGPASSWORD=${POSTGRES_PASSWORD} && /usr/bin/pg_dump -U ${POSTGRES_USER} ${DB_NAME}" | gzip -9 >"${DUMP_PATH}/postgres-backup-${DB_NAME}.sql.gz"
+    gzip -d "${DUMP_PATH}/postgres-backup-${DB_NAME}.sql.gz"
+  done
+
+  printf "Shutting down iparapheur..."
+  docker compose down -v
+
+  tar -czf "${DUMP_PATH}".tar.gz "${DUMP_PATH}"
+  rm -r "${DUMP_PATH}"
+
+  chown 6789:6789 "${DUMP_PATH}".tar.gz
+
+  printf "DUMP complete -> %s.\n" "${DUMP_PATH}"
+}
+
+__main__ "${@}"
