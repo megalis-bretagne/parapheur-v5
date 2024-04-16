e3d018ac4f8b9364b9f665f2e9f4c92f192d57ac
Adrien BRICCHI <adrien.bricchi@libriciel.coop>
2024-04-16 11:00:21 +0200

    Fix some licenses

--- src/main/resources/docker-resources/libriciel-auto-update.sh
+++ src/main/resources/docker-resources/libriciel-auto-update.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
 #
-# iparapheur
-# Copyright (C) 2019-2024 Libriciel SCOP
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-
 # Libriciel Auto-update
 # Copyright (C) 2024 Libriciel-SCOP
 #
@@ -33,6 +16,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 set -e
 
